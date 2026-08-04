d4303d3d678bc137c14333db999794583d9896ec
marcos <msrserpa@gmail.com>
2026-08-04 13:00:36 -0300

    cuida

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Metodos.verificarAnaclico("Roma","Amor"));
-        System.out.println(Metodos.verificarAnagrama("casa","caso"));
+        System.out.println(Metodos.verificarAnagrama("Vaja","Java"));
 
     }
 }
--- src/Metodos.java
+++ src/Metodos.java
@@ -18,23 +18,25 @@ public class Metodos {
 
     }
 
-    public static boolean verificarAnagrama(String primeiraPalavra,String segundaPalavra){
-        boolean resultado = true;
+    public static Boolean verificarAnagrama(String primeiraPalavra,String segundaPalavra){
+        Boolean resultado = true;
+        String palavraMinimizada1 = primeiraPalavra.toLowerCase();
+        String palavraMinimizada2 = segundaPalavra.toLowerCase();
 
-        if (primeiraPalavra.length() != segundaPalavra.length()){
+        if (palavraMinimizada1.length() != palavraMinimizada2.length()){
             resultado = false;
         }else{
-            for (int i = 0;i <= segundaPalavra.length()-1;i++){
+            for (int i = 0;i <= palavraMinimizada1.length()-1;i++){
 
-                char letra = segundaPalavra.charAt(i);
-                String letraConvertida = String.valueOf(letra);
+                char letra2 = palavraMinimizada2.charAt(i);
+                String letraConvertida2 = String.valueOf(letra2);
+                char letra1 = palavraMinimizada1.charAt(i);
+                String letraConvertida1 = String.valueOf(letra1);
 
-                if (!(primeiraPalavra.contains(letraConvertida))){
+                if (!palavraMinimizada1.contains(letraConvertida2) || !palavraMinimizada2.contains(letraConvertida1)){
                     resultado = false;
-                    System.out.println("Não existe essa letra na segunda palavra");
                     break;
                 }
-
             }
         }
 
